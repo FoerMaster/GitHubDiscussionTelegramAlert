5ee28521133ee5501cffb362755249f674156121
Foer <39239388+FoerMaster@users.noreply.github.com>
2026-01-18 04:34:55 +0500

    feat: Добавлена валидация по секрету

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	githubEnums "discus.TelegramAlert/enum"
 	"discus.TelegramAlert/models"
@@ -15,6 +20,23 @@ import (
 type Response struct {
 	Status  string `json:"status"`
 	Message string `json:"message"`
+}
+
+func validateGitHubSignature(signature string, body []byte) bool {
+	if signature == "" {
+		return false
+	}
+
+	if !strings.HasPrefix(signature, "sha256=") {
+		return false
+	}
+	signatureHash := strings.TrimPrefix(signature, "sha256=")
+
+	mac := hmac.New(sha256.New, []byte(os.Getenv("GITHUB_WEBHOOK_SECRET")))
+	mac.Write(body)
+	expectedHash := hex.EncodeToString(mac.Sum(nil))
+
+	return hmac.Equal([]byte(signatureHash), []byte(expectedHash))
 }
 
 func processHealth(w http.ResponseWriter, r *http.Request) {
@@ -37,11 +59,25 @@ func processGitHub(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	bodyBytes, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Error reading request body", http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+
+	// Check signature for security (Secret in GitHub webhook settings)
+	signature := r.Header.Get("X-Hub-Signature-256")
+	if !validateGitHubSignature(signature, bodyBytes) {
+		http.Error(w, "Invalid signature", http.StatusUnauthorized)
+		return
+	}
+
 	eventType := githubEnums.GitHubEvent(r.Header.Get("x-github-event"))
 
 	var body models.GitHubWebhook
-	err := json.NewDecoder(r.Body).Decode(&body)
-	if err != nil {
+	errBody := json.NewDecoder(r.Body).Decode(&body)
+	if errBody != nil {
 		http.Error(w, "Error parsing JSON", http.StatusBadRequest)
 		return
 	}
